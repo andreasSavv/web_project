77cd56adbfaf56c75ff91d018b04cbb7c535d4f9
IoannisKafkalias <166229132+IoannisKafkalias@users.noreply.github.com>
2025-12-08 09:17:51 +0200

    Update professor_page2.php

--- SQL/php/professor/professor_page2.php
+++ SQL/php/professor/professor_page2.php
@@ -17,20 +17,18 @@ $name = $user['professor_name'] ?? "Καθηγητής";
 <!DOCTYPE html>
 <html lang="el">
 <head>
-    <meta charset="UTF-8">
-    <title>Πίνακας Ελέγχου Διδάσκοντα</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-     <style>
-        body { font-family: Arial; background: #f4f4f4; margin: 40px; }
-        .container { background: white; padding: 20px; border-radius: 10px; max-width: 800px; margin: auto; box-shadow: 0 0 10px rgba(0,0,0,0.2); }
-        a { color: #007bff; text-decoration: none; }
-        a:hover { text-decoration: underline; }
-    </style>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <title>Αρχική Σελίδα Καθηγητή </title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <style>
+    .sidebar { min-height: 100vh; }
+    @media (max-width: 767.98px){
+      main { padding-top: 1rem; }
+    }
+  </style>
 </head>
-
 <body>
-
 <div class="container">
 
     <h2 class="mb-4">Καλωσήρθες, <span><?php echo htmlspecialchars($name); ?></span>!</h2>
