867c054f9f42ed22c159a307f72c95f2c0a9a0db
IoannisKafkalias <166229132+IoannisKafkalias@users.noreply.github.com>
2025-12-08 10:56:47 +0200

    Update professor_page.php

--- SQL/php/professor/professor_page.php
+++ SQL/php/professor/professor_page.php
@@ -31,9 +31,23 @@ $name = $user['professor_name'] ?? "Καθηγητής";
     <h2>Καλωσήρθες, Καθηγητά <?php echo htmlspecialchars($name); ?>!</h2>
     <p>Είσαι συνδεδεμένος επιτυχώς στο σύστημα.</p>
     <ul>
-        <li><a href="diplomas.php">Διπλωματικές Εργασίες</a></li>
-        <li><a href="add_diploma.php">Προσθήκη Νέας Διπλωματικής</a></li>
-        <li><a href="edit_diploma.php">Επεξεργασία Διπλωματικής</a></li>
+        <li><a href="add_diploma.php">1)Δημιουργία Διπλωματικής Εργασίας</a></li>
+        <li><a href="diplo_assign.php">2)Ανάθεση Διπλωματικής Εργασίας</a></li>
+        <li><a href="pending_inv.php">3)Προβολή προσκλήσεων σε τριμελή</a></li>
+        <li><a href="diplomas.php">4)Διπλωματικές Εργασίες</a></li>
+        <li><a href="pr_cancel_diplo.php">5)Ακύρωση Ενεργής Διπλωματικής Εργασίας</a></li>
+        <li><a href="cancel_pending_diplo.php">6)Ακύρωση "Υπο Ανάθεση" Διπλωματικής Εργασίας</a></li>
+        <li><a href="diplo_status_under_rev.php">7)Αλλαγή Κατάστασης ΔΕ σε Περατωμένη</a></li>
+        <li><a href="prof_notes.php">8)Σημειώσεις Διπλωματικής Εργασίας</a></li>
+        <li><a href="grade_enable.php">9)Ενεργοποίηση Βαθμού</a></li>
+        <li><a href="diplo_grade.php">10)Καταχώρηση Βαθμού</a></li>
+        <li><a href="prof_graphs.php">11)Γραφικές Παραστάσεις Καθηγητή</a></li>
+        <li><a href="prof_show_invite.php">12)Προβολή Καθηγητών που εχουν προσκληθεί ως μέλος τριμελούς</a></li>
+        <li><a href="edit_diploma.php">13)Επεξεργασία Διπλωματικής</a></li>
+        <li><a href="prof_show_notes.php">14)Σημειώσεις Διπλωματικών Εργασιών</a></li>
+        <li><a href="prof_show_grades.php">15)Βαθμοί ΔΕ ως μέλος τριμελούς</a></li>
+        <li><a href="prof_show_st_notes.php">16)Προβολή πρόχειρου κειμένου φοιτητή ως μέλος τριμελούς</a></li>
+        <li><a href="announcements.php">17)Δημιουργία ανακοίνωσης</a></li>
         <li><a href="logout.php">Αποσύνδεση</a></li>
         
     </ul>
